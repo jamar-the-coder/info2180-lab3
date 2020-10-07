37cfa4601df02dd751299ef998fc1d4d0ddbd339
Jamar Lee <jamar.lee30@gmail.com>
2020-10-07 11:17:15 -0500

    Implemented Tic-Tac-Toe basics.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,59 @@
 window.onload = function (){
 	
+	var turn = 1;
+	
+	
 	var gridDoc = document.querySelectorAll("#board div");
+	
+	emptyArr = [];
+	
 		for ( var i = 0; i < gridDoc.length; i++ ){
 			
 			gridDoc[i].classList.add("square");
 				console.log(gridDoc);
+				
+				gridDoc[i].addEventListener("click", function(){
+					
+							
+							if (turn == 1){
+								
+								this.innerHTML = "X";
+								this.classList.add("X");
+							
+							turn = 2;
+							
+							}
+							else{
+								this.innerHTML = "O";
+								this.classList.add("O");
+								
+								turn = 1;
+									
+									
+									
+								}
+								
+								
+
+
+
+	});
+	
 		}
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 }
